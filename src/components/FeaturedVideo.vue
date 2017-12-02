033b5a07f7d638b9bafde47bb4190ef031207601
laurabmackenzie <mackenz7@seattleu.edu>
2017-12-02 10:58:33 -0800

    Updated FeaturedVideo

--- src/components/FeaturedVideo.vue
+++ src/components/FeaturedVideo.vue
@@ -3,8 +3,8 @@
     <p class="links"><router-link v-bind:to="{name:'SunTracker'}">Return to SunTracker</router-link></p>
     <div class=video>
     <h1>Winter Solstice<br>Fairbanks, Alaska</h1>
-    <iframe width="420" height="236" src="https://www.youtube.com/embed/Olbo4Am4U44?rel=0&amp;controls=0&amp;showinfo=0&amp;start=7" frameborder="0" allowfullscreen></iframe> 
-    <p>December 21, 2012<br>Sunrise: 11:06 AM<br>Sunset: 2:59 PM<br><br>Time lapse video created by Taro Nakai</p>
+    <iframe width="504" height="284" src="https://www.youtube.com/embed/Olbo4Am4U44?rel=0&amp;controls=0&amp;showinfo=0&amp;start=7" frameborder="0" allowfullscreen></iframe> 
+    <p>December 21, 2012<br>Sunrise: 11:06 AM | Sunset: 2:59 PM <br>Time lapse video created by Taro Nakai</p>
 </div>
 </div>
 </template>
@@ -28,7 +28,7 @@ h1 {
 }
 @media screen and (min-width:481px) and (max-width: 768px) {
 h1 {
-  font-size:26px;
+  font-size:24px;
 }
 }
 @media screen and (min-width:769px) and (max-width: 1024px) {
